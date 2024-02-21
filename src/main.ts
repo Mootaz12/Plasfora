5e823168f49ad0a8359be5357d7266c5ba1390bd
Mootaz Belleh Weslati <mootazbellehweslati@gmail.com>
2024-02-21 16:49:38 +0100

    Implement signup functionality

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { connect } from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
+const run = async () => {
+  await connect('mongodb://127.0.0.1:27017/plasfora');
+  console.log('Connected to plasfora');
+};
+
+run().catch((err) => console.error(err));
 bootstrap();
